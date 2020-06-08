7843f3c0fad877108ae857addfc1414f3e59ac83
Sebastian Busch <s.busch@obg-gruppe.de>
2020-06-08 10:32:38 +0200

    stripped down further

--- scripts/build.js
+++ scripts/build.js
@@ -1,41 +1,129 @@
-'use strict';
+"use strict";
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = "production";
+process.env.NODE_ENV = "production";
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on("unhandledRejection", (err) => {
   throw err;
 });
 
-const path = require('path');
-const chalk = require('react-dev-utils/chalk');
-const fs = require('fs-extra');
-const webpack = require('webpack');
-const configFactory = require('../config/webpack.config');
-const paths = require('../config/paths');
-const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
-const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
-const printBuildError = require('react-dev-utils/printBuildError');
+const path = require("path");
+const chalk = require("react-dev-utils/chalk");
+const fs = require("fs-extra");
+const webpack = require("webpack");
+const formatWebpackMessages = require("react-dev-utils/formatWebpackMessages");
+const printHostingInstructions = require("react-dev-utils/printHostingInstructions");
+const printBuildError = require("react-dev-utils/printBuildError");
 
-const isInteractive = process.stdout.isTTY;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-// Warn and crash if required files are missing
-if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
-  process.exit(1);
-}
+// Make sure any symlinks in the project folder are resolved:
+// https://github.com/facebook/create-react-app/issues/637
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
-// Generate configuration
-const config = configFactory('production');
+// config after eject: we're in ./config/
+const paths = {
+  appPath: resolveApp("."),
+  appBuild: resolveApp("build"),
+  appPublic: resolveApp("public"),
+  appHtml: resolveApp("public/index.html"),
+  appPackageJson: resolveApp("package.json"),
+  appSrc: resolveApp("src"),
+};
+
+console.log(paths);
+// throw new Error('sdf');
+
+const webpackConfig = {
+  mode: "production",
+  // Stop compilation early in production
+  bail: true,
+  devtool: "source-map",
+  // These are the "entry points" to our application.
+  // This means they will be the "root" imports that are included in JS bundle.
+  entry: ["./src/index.jsx"],
+  output: {
+    // The build folder.
+    path: paths.appBuild,
+    // Add /* filename */ comments to generated require()s in the output.
+    pathinfo: false,
+    // There will be one main bundle, and one file per asynchronous chunk.
+    // In development, it does not produce real files.
+    filename: "static/js/[name].js",
+    // TODO: remove this when upgrading to webpack 5
+    futureEmitAssets: true,
+    // There are also additional JS chunk files if you use code splitting.
+    chunkFilename: "static/js/[name].chunk.js",
+    // webpack uses `publicPath` to determine where the app is being served from.
+    // It requires a trailing slash, or the file assets will get an incorrect path.
+    // We inferred the "public path" (such as / or /my-project) from homepage.
+    publicPath: "/",
+    // Point sourcemap entries to original disk location (format as URL on Windows)
+    devtoolModuleFilenameTemplate: (info) =>
+      path
+        .relative(paths.appSrc, info.absoluteResourcePath)
+        .replace(/\\/g, "/"),
+    // this defaults to 'window', but by setting it to 'this' then
+    // module chunks which are built will work in web workers as well.
+    globalObject: "this",
+  },
+  optimization: {
+    minimize: false, // FIXME: was isEnvProduction,
+    // Automatically split vendor and commons
+    // https://twitter.com/wSokra/status/969633336732905474
+    // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+    splitChunks: {
+      chunks: "all",
+      name: false,
+    },
+    // Keep the runtime chunk separated to enable long term caching
+    // https://twitter.com/wSokra/status/969679223278505985
+    // https://github.com/facebook/create-react-app/issues/5358
+    runtimeChunk: {
+      name: (entrypoint) => `runtime-${entrypoint.name}`,
+    },
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      // Process application JS with Babel.
+      // The preset includes JSX, Flow, TypeScript, and some ESnext features.
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        include: paths.appSrc,
+        loader: require.resolve("babel-loader"),
+        options: {
+          // Disable caching results in ./node_modules/.cache/babel-loader/ directory for faster rebuilds.
+          // (feature of `babel-loader` for webpack (not Babel itself))
+          cacheDirectory: false,
+          // See #6846 for context on why cacheCompression is disabled
+          cacheCompression: false,
+          compact: false,
+        },
+      },
+    ],
+  },
+  plugins: [
+    // Generates an `index.html` file with the <script> injected.
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: paths.appHtml,
+      minify: false,
+    }),
+  ],
+  // Turn off performance processing
+  performance: false,
+};
 
 // We require that you explicitly set browsers and do not fall back to
 // browserslist defaults.
-const { checkBrowsers } = require('react-dev-utils/browsersHelper');
-checkBrowsers(paths.appPath, isInteractive)
+const { checkBrowsers } = require("react-dev-utils/browsersHelper");
+checkBrowsers(paths.appPath, true) // isInteractive == true
   .then(() => {
     // Remove all content but keep the directory so that
     // if you're in it, you don't end up in Trash
@@ -46,42 +134,32 @@ checkBrowsers(paths.appPath, isInteractive)
     return build();
   })
   .then(
-    ({ stats, warnings }) => {
+    ({ _stats_, warnings }) => {
       if (warnings.length) {
-        console.log(chalk.yellow('Compiled with warnings.\n'));
-        console.log(warnings.join('\n\n'));
-        console.log(
-          '\nSearch for the ' +
-            chalk.underline(chalk.yellow('keywords')) +
-            ' to learn more about each warning.'
-        );
-        console.log(
-          'To ignore, add ' +
-            chalk.cyan('// eslint-disable-next-line') +
-            ' to the line before.\n'
-        );
+        console.log(chalk.yellow("Compiled with warnings.\n"));
+        console.log(warnings.join("\n\n"));
       } else {
-        console.log(chalk.green('Compiled successfully.\n'));
+        console.log(chalk.green("Compiled successfully.\n"));
       }
 
       const appPackage = require(paths.appPackageJson);
-      const publicPath = config.output.publicPath;
+      const publicPath = webpackConfig.output.publicPath;
       const buildFolder = path.relative(process.cwd(), paths.appBuild);
       printHostingInstructions(
         appPackage,
-        '/',
+        "/",
         publicPath,
         buildFolder,
         true // useYarn
       );
     },
-    err => {
-      console.log(chalk.red('Failed to compile.\n'));
+    (err) => {
+      console.log(chalk.red("Failed to compile.\n"));
       printBuildError(err);
       process.exit(1);
     }
   )
-  .catch(err => {
+  .catch((err) => {
     if (err && err.message) {
       console.log(err.message);
     }
@@ -90,9 +168,9 @@ checkBrowsers(paths.appPath, isInteractive)
 
 // Create the production build and print the deployment instructions.
 function build() {
-  console.log('Creating an optimized production build...');
+  console.log("Creating an optimized production build...");
 
-  const compiler = webpack(config);
+  const compiler = webpack(webpackConfig);
   return new Promise((resolve, reject) => {
     compiler.run((err, stats) => {
       let messages;
@@ -102,13 +180,6 @@ function build() {
         }
 
         let errMessage = err.message;
-
-        // Add additional information for postcss errors
-        if (Object.prototype.hasOwnProperty.call(err, 'postcssNode')) {
-          errMessage +=
-            '\nCompileError: Begins at CSS selector ' +
-            err['postcssNode'].selector;
-        }
 
         messages = formatWebpackMessages({
           errors: [errMessage],
@@ -125,23 +196,8 @@ function build() {
         if (messages.errors.length > 1) {
           messages.errors.length = 1;
         }
-        return reject(new Error(messages.errors.join('\n\n')));
+        return reject(new Error(messages.errors.join("\n\n")));
       }
-      if (
-        process.env.CI &&
-        (typeof process.env.CI !== 'string' ||
-          process.env.CI.toLowerCase() !== 'false') &&
-        messages.warnings.length
-      ) {
-        console.log(
-          chalk.yellow(
-            '\nTreating warnings as errors because process.env.CI = true.\n' +
-              'Most CI servers set it automatically.\n'
-          )
-        );
-        return reject(new Error(messages.warnings.join('\n\n')));
-      }
-
       return resolve({
         stats,
         warnings: messages.warnings,
@@ -153,6 +209,6 @@ function build() {
 function copyPublicFolder() {
   fs.copySync(paths.appPublic, paths.appBuild, {
     dereference: true,
-    filter: file => file !== paths.appHtml,
+    filter: (file) => file !== paths.appHtml,
   });
 }
