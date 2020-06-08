a4c47e56de204fa1ad00c2c3955782fd7225d8e4
Sebastian Busch <s.busch@obg-gruppe.de>
2020-06-08 12:37:41 +0200

    cleanup

--- scripts/build.js
+++ scripts/build.js
@@ -4,9 +4,7 @@
 process.env.BABEL_ENV = "production";
 process.env.NODE_ENV = "production";
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+// Makes the script crash on unhandled rejections instead of silently ignoring them.
 process.on("unhandledRejection", (err) => {
   throw err;
 });
@@ -25,74 +23,39 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
-// config after eject: we're in ./config/
-const paths = {
-  appPath: resolveApp("."),
-  appBuild: resolveApp("build"),
-  appPublic: resolveApp("public"),
-  appHtml: resolveApp("public/index.html"),
-  appPackageJson: resolveApp("package.json"),
-  appSrc: resolveApp("src"),
-};
+const buildPath = resolveApp("build");
+const srcPath = resolveApp("src");
 
-// console.log(paths);
+// console.log("paths", { buildPath, srcPath });
 
 const webpackConfig = {
   mode: "production",
   // Stop compilation early in production
   bail: true,
   devtool: "source-map",
-  // These are the "entry points" to our application.
-  // This means they will be the "root" imports that are included in JS bundle.
   entry: ["./src/index.jsx"],
   output: {
-    // The build folder.
-    path: paths.appBuild,
-    // Add /* filename */ comments to generated require()s in the output.
-    pathinfo: false,
-    // There will be one main bundle, and one file per asynchronous chunk.
-    // In development, it does not produce real files.
-    filename: "static/js/[name].js",
-    // TODO: remove this when upgrading to webpack 5
+    path: buildPath,
     futureEmitAssets: true,
-    // There are also additional JS chunk files if you use code splitting.
+    filename: "static/js/[name].js",
     chunkFilename: "static/js/[name].chunk.js",
-    // webpack uses `publicPath` to determine where the app is being served from.
-    // It requires a trailing slash, or the file assets will get an incorrect path.
-    // We inferred the "public path" (such as / or /my-project) from homepage.
-    publicPath: "/",
     // Point sourcemap entries to original disk location (format as URL on Windows)
     devtoolModuleFilenameTemplate: (info) =>
-      path
-        .relative(paths.appSrc, info.absoluteResourcePath)
-        .replace(/\\/g, "/"),
-    // this defaults to 'window', but by setting it to 'this' then
-    // module chunks which are built will work in web workers as well.
-    globalObject: "this",
+      path.relative(srcPath, info.absoluteResourcePath).replace(/\\/g, "/"),
   },
   optimization: {
     minimize: false,
-    // Automatically split vendor and commons
-    // https://twitter.com/wSokra/status/969633336732905474
-    // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
     splitChunks: {
       chunks: "all",
-      name: false,
     },
-    // Keep the runtime chunk separated to enable long term caching
-    // https://twitter.com/wSokra/status/969679223278505985
-    // https://github.com/facebook/create-react-app/issues/5358
-    runtimeChunk: {
-      name: (entrypoint) => `runtime-${entrypoint.name}`,
-    },
+    runtimeChunk: true,
   },
   module: {
     strictExportPresence: true,
     rules: [
-      // Process application JS with Babel.
       {
         test: /\.(js|jsx)$/,
-        include: paths.appSrc,
+        include: srcPath,
         loader: require.resolve("babel-loader"),
         options: {
           plugins: [
@@ -119,7 +82,6 @@ const webpackConfig = {
   plugins: [
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
-      // template: paths.appHtml,
       templateContent: `<!DOCTYPE html>
       <html lang="en">
         <head>
@@ -133,16 +95,38 @@ const webpackConfig = {
       minify: false,
     }),
   ],
-  // Turn off performance processing
   performance: false,
 };
 
-// Remove all content but keep the directory so that
-// if you're in it, you don't end up in Trash
-fs.emptyDirSync(paths.appBuild);
+// Remove all content of build path
+fs.emptyDirSync(buildPath);
 
 // Start the webpack build
-build()
+new Promise((resolve, reject) => {
+  webpack(webpackConfig).run((err, stats) => {
+    let messages;
+    if (err) {
+      if (!err.message) {
+        return reject(err);
+      }
+
+      let errMessage = err.message;
+
+      messages = formatWebpackMessages({
+        errors: [errMessage],
+        warnings: [],
+      });
+    } else {
+      messages = formatWebpackMessages(
+        stats.toJson({ all: false, warnings: true, errors: true })
+      );
+    }
+    if (messages.errors.length) {
+      return reject(new Error(messages.errors.join("\n\n")));
+    }
+    return resolve(messages.warnings);
+  });
+})
   .then(
     (warnings) => {
       if (warnings.length) {
@@ -166,36 +150,4 @@ build()
   });
 
 // Create the production build and print the deployment instructions.
-function build() {
-  const compiler = webpack(webpackConfig);
-  return new Promise((resolve, reject) => {
-    compiler.run((err, stats) => {
-      let messages;
-      if (err) {
-        if (!err.message) {
-          return reject(err);
-        }
-
-        let errMessage = err.message;
-
-        messages = formatWebpackMessages({
-          errors: [errMessage],
-          warnings: [],
-        });
-      } else {
-        messages = formatWebpackMessages(
-          stats.toJson({ all: false, warnings: true, errors: true })
-        );
-      }
-      if (messages.errors.length) {
-        // Only keep the first error. Others are often indicative
-        // of the same problem, but confuse the reader with noise.
-        if (messages.errors.length > 1) {
-          messages.errors.length = 1;
-        }
-        return reject(new Error(messages.errors.join("\n\n")));
-      }
-      return resolve(messages.warnings);
-    });
-  });
-}
+function build() {}
